a88eb6a6d3624b7b33209930f73864ee43cdfb62
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-04-05 13:58:31 +0800

    RED: map > dispatches to objects that implement 'map'

--- src/map.test.js
+++ src/map.test.js
@@ -28,4 +28,13 @@ describe('map', () => {
         var h = map(f, g);
         expect(h(10)).toEqual(10 * 2 - 1);
     });
+    it('dispatches to objects that implement `map`', function () {
+        var obj = {
+            x: 100,
+            map: function (f) {
+                return f(this.x);
+            }
+        };
+        expect(map(add1, obj)).toEqual(101);
+    });
 });
